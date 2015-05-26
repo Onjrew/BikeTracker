7a637e9c7a4c31e06f0322c4e5bdc13a8ace7692
Andrew Ferguson <andrew.scott.ferguson@gmail.com>
2015-05-25 22:25:31 -0400

    Configured the RTC clock on the DataLogger shield.
    The RTC now holds about the correct time as my Mac. The only small change left is to make the SD card filenames the date and log the time stamp. Once that is done the Magnet interrupt can be implemented.

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,8 +1,63 @@
 
+#include <Time.h>
+#include <Wire.h>
+#include <SD.h>
+#include <DS1307RTC.h>
+
+
+const int chipSelect = 10;
+tmElements_t tm;
+
 void setup()
-{
+{ 
+
+  bool config = false;
+  tmElements_t tm_copy;
+
+  Serial.begin(9600);
+  
+  Serial.print("Initializing SD Card... ");
+  if (!SD.begin(chipSelect)){
+    Serial.println("card initialization failed...");
+    return;
+  }
+  
+  Serial.println("card initialized.");
+
+
 }
 
 void loop()
 {
+  char *datestamp; 
+  tmElements_t tm;
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  if (dataFile){
+    if (RTC.read(tm)) {
+
+      Serial.print(tmYearToCalendar(tm.Year));
+      Serial.print("/");
+      Serial.print(tm.Month);
+      Serial.print("/");
+      Serial.print(tm.Day);
+      Serial.print(" - ");
+
+      print2digits(tm.Hour);
+      Serial.print(" : ");
+      print2digits(tm.Minute);
+      Serial.print(" : ");
+      print2digits(tm.Second);
+      Serial.println();
+    }
+  }
+  dataFile.close();
+  delay(1000);
+}
+
+void print2digits(int number) {
+  if (number >= 0 && number<10) {
+    Serial.write('0');
+  }
+  Serial.print(number);
 }
