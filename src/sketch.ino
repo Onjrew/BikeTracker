37356842b34eb6d83763c1bc78158f75ec7db77b
Andrew Ferguson <andrew.scott.ferguson@gmail.com>
2015-06-22 21:23:41 -0400

    I added proper file creation and time stamping.
    I moved the file creation and inital time stamping to the setup() function. I cleaned up some of the Serial output.

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,18 +1,45 @@
 
+#include <stdlib.h>
 #include <Time.h>
 #include <Wire.h>
 #include <SD.h>
 #include <DS1307RTC.h>
 
-
 const int chipSelect = 10;
 tmElements_t tm;
+char filename[13];
+File data_file;
+int year_num;
+int month_num;
+int day_num;
+int hour_num;
+int minute_num;
+int second_num;
+char year_str[5];
+char month_str[3];
+char day_str[3];
+char hour_str[3];
+char minute_str[3];
+char second_str[3];
+char date_stamp[13];
+char time_stamp[13];
+
+void make_2digit_str(int num, char *str)
+{
+  char tmp1[3];
+  char tmp2[3];
+  if (num<10){
+    dtostrf(num, 1, 0, tmp1);
+    sprintf(tmp2, "0%s", tmp1);
+    strcpy(str, tmp2);
+  } else {
+    dtostrf(num, 2, 0, str);
+  }
+}
 
 void setup()
 { 
-
-  bool config = false;
-  tmElements_t tm_copy;
+  
 
   Serial.begin(9600);
   
@@ -24,40 +51,67 @@ void setup()
   
   Serial.println("card initialized.");
 
+  RTC.read(tm);
+
+  year_num  = tmYearToCalendar(tm.Year);
+  month_num = tm.Month;
+  day_num   = tm.Day;
+
+  dtostrf(year_num, 4, 0, year_str);
+
+  make_2digit_str(month_num, month_str);
+  make_2digit_str(day_num, day_str);
+
+  hour_num = tm.Hour;
+  minute_num = tm.Minute;
+  second_num = tm.Second;
+
+  make_2digit_str(hour_num, hour_str);
+  make_2digit_str(minute_num, minute_str);
+  make_2digit_str(second_num, second_str);
+
+  sprintf(date_stamp, "%s%s%s", year_str, month_str, day_str);
+  sprintf(time_stamp, "%s%s%s", hour_str, minute_str, second_str);
+  sprintf(filename, "%s%s%s%s.log", month_str, day_str, hour_str, minute_str);  
+
+  data_file = SD.open(filename, FILE_WRITE);
+  if (data_file){
+    Serial.print("Successfully created file: ");
+    Serial.println(filename);
+    data_file.write(time_stamp);
+    data_file.close();
+  } else {
+    Serial.print("Failed to create file: ");
+    Serial.println(filename);
+  }
 
 }
+
 
 void loop()
 {
-  char *datestamp; 
-  tmElements_t tm;
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  data_file = SD.open(filename, FILE_WRITE);
 
-  if (dataFile){
+  if (data_file){
     if (RTC.read(tm)) {
 
-      Serial.print(tmYearToCalendar(tm.Year));
-      Serial.print("/");
-      Serial.print(tm.Month);
-      Serial.print("/");
-      Serial.print(tm.Day);
-      Serial.print(" - ");
+      hour_num = tm.Hour;
+      minute_num = tm.Minute;
+      second_num = tm.Second;
 
-      print2digits(tm.Hour);
-      Serial.print(" : ");
-      print2digits(tm.Minute);
-      Serial.print(" : ");
-      print2digits(tm.Second);
-      Serial.println();
+      make_2digit_str(hour_num, hour_str);
+      make_2digit_str(minute_num, minute_str);
+      make_2digit_str(second_num, second_str);
+
+      sprintf(time_stamp, ",%s%s%s", hour_str, minute_str, second_str);
+      
+      data_file.write(time_stamp);
     }
+  } else {
+    Serial.print("Could not create data_file: ");
+    Serial.println(filename);
   }
-  dataFile.close();
-  delay(1000);
+  data_file.close();
+  delay(10000);
 }
 
-void print2digits(int number) {
-  if (number >= 0 && number<10) {
-    Serial.write('0');
-  }
-  Serial.print(number);
-}
